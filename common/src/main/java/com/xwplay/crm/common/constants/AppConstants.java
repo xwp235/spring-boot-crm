7120c69c36bbdf9d593d246e112987efcbdc32ee
xiao.wenpeng <ja_xwp@yeah.net>
2025-03-13 15:45:07 +0900

    chore: 初始化项目

---
+++ common/src/main/java/com/xwplay/crm/common/constants/AppConstants.java
@@ -0,0 +1,12 @@
+package com.xwplay.crm.common.constants;
+
+public interface AppConstants {
+
+    String X_REQUESTED_ID = "X-Requested-Id";
+    String X_AUTHENTICATE = "X-Authenticate";
+
+    interface MDC {
+        String LOG_ID = "logId";
+    }
+
+}
