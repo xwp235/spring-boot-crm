7a55da0c003847ecb99efade06ed0bc84fb05382
xiao.wenpeng <ja_xwp@yeah.net>
2025-04-02 17:08:47 +0900

    feat: 添加upload-chunk示例接口

--- platform/src/main/java/com/xwplay/crm/platform/controller/TestController.java
+++ platform/src/main/java/com/xwplay/crm/platform/controller/TestController.java
@@ -1,10 +1,17 @@
 package com.xwplay.crm.platform.controller;
 
+import com.xwplay.crm.common.resp.JsonResp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 import java.util.Date;
@@ -26,6 +33,25 @@ public class TestController {
                 "zd1", zd1,
                 "localDateTime1", ld1, "double1", 1.24,
                 "float1", 1.1f, "long1", 100L);
+    }
+
+    private static final String TEMP_DIR = "D:\\upload\\";
+
+    @PostMapping("upload-chunk")
+    public JsonResp uploadChunk(
+            @RequestParam MultipartFile chunkFile,
+            @RequestParam int chunkIndex,
+            @RequestParam String filename
+    ) throws IOException {
+        var chunkFilepath = TEMP_DIR + filename;
+        var chunkFileDir = Paths.get(chunkFilepath);
+        if (!Files.exists(chunkFileDir)) {
+            Files.createDirectories(chunkFileDir);
+        }
+        var splitChunkFilepath = Paths.get(chunkFilepath, "chunk_" + chunkIndex);
+        var createdSplitChunkFilepath = Files.createFile(splitChunkFilepath);
+        Files.write(createdSplitChunkFilepath, chunkFile.getBytes());
+        return JsonResp.ok();
     }
 
 }
